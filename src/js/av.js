a596969b54d05a2824cba54d94699449b4c22f9b
yyhsz <591985864@qq.com>
2019-03-27 20:17:30 +0800

    写下雏形

---
+++ src/js/av.js
@@ -0,0 +1,7 @@
+var APP_ID = 'LFhkyUzmvD0OjDc1cTppDh4c-gzGzoHsz';
+        var APP_KEY = 'A0st43qlwi9RSkgkCkeJOglt';
+
+        AV.init({
+            appId: APP_ID,
+            appKey: APP_KEY
+        });
