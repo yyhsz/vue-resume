c85138d3cf8dfd8ba74de1de8444221947def73c
yyhsz <591985864@qq.com>
2019-03-29 16:57:19 +0800

    添加登出功能

--- src/js/app.js
+++ src/js/app.js
@@ -8,24 +8,68 @@ let vm = new Vue({
             job: '前端工程师',
             email: '12332@qq.com',
             phone: '2348923758',
-            
+
         },
-        loginVisible:false,
-        signUpVisible:false
+        loginVisible: false, //登陆
+        signUpVisible: false,//注册
+        signUp: {
+            userName: '',
+            password: '',
+            matchPassword: '',
+        },
+        login: {
+            userName: '',
+            password: '',
+        }
     },
     methods: {
         onEdit(key, value, x) {
             console.log(x)
             this.resume[key] = value
         },
-        clickSave(){
+        clickSave() {
             let currentUser = AV.User.current()
-            if(!currentUser){
+            if (!currentUser) {
                 this.loginVisible = true
-            }else{
-                this.saveResume()
+            } else {
+                this.saveResume(AV.User.current().id)
             }
         },
+        saveResume(id){
+            var user = AV.Object.createWithoutData('User', id);
+            user.set('resume',this.resume);
+            user.save();
+        },
+        onSignUp(e) {
+            let user = new AV.User();
+            user.setUsername(this.signUp.userName);
+            user.setPassword(this.signUp.password);
+            user.signUp().then((loggedInUser) => {
+                this.signUp.userName = '';
+                this.signUp.password = '';
+                this.signUp.matchPassword = '';
+                this.loginVisible = true;
+                this.signUpVisible = false;
+                AV.User.logOut();
+            }).catch((x) => {
+                console.log(x)
+            })
+        },
+        onLogin(e) {
+            AV.User.logIn(this.login.userName, this.login.password).then(function (loggedInUser) {
+                console.log(loggedInUser);
+                this.loginVisible = false;
+            }).catch(function (error) {
+                if(error.code === 211) alert('用户名不存在')
+            });
+        },
+        onLogout(){
+            AV.User.logOut();
+            window.location.reload()
+        }
 
     }
 })
+
+
+
